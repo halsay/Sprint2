3f074e399fa732011245a05bf4e780c3acbd5fc5
jackey <jackey1510@gmail.com>
2017-04-04 22:47:10 +0100

    added map and player movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cam.orthographicSize = Screen.height / 200f;
+	}
+}

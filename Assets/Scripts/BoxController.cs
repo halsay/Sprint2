e6cca031d84327f75a31de1f1599db2907940f8e
jackey <jackey1510@gmail.com>
2017-04-17 19:36:29 +0100

    trap

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour {
+
+    public GameObject closedDia, openDia;
+    private bool isOpen;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        isOpen = false;
+        anim = GetComponent<Animator>();
+        openDia.SetActive(false);
+        closedDia.SetActive(true);
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Little" && !isOpen && Input.GetKeyUp(KeyCode.Space))
+        {
+            isOpen = true;
+            closedDia.SetActive(false);
+            openDia.SetActive(true);
+            anim.SetBool("isOpen", true);
+        }
+    }
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+    private Animator anim;
+    private DialogueManager dMan;
+    public string[] dialogLines;
+    public string[] speakers;
+    public Animator troll;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Little")
+        {
+            if (!dMan.dialogActive)
+            {
+                dMan.dialogLines = dialogLines;
+                dMan.speakers = speakers;
+                dMan.currentLine = 0;
+                dMan.ShowDialogue();
+            }
+
+            yield return new WaitForSeconds(1f);
+            anim.SetTrigger("activated");
+            GameObject.FindGameObjectWithTag("Little").SetActive(false);
+            GameObject.FindGameObjectWithTag("Middle").SetActive(false);
+            GameObject.FindGameObjectWithTag("Big").SetActive(false);
+            yield return new WaitForSeconds(2f);
+            troll.SetTrigger("appear");
+            
+        }
+    }
+}

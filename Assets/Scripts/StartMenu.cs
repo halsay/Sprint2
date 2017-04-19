704431426f25755512607951681709db85896efc
jackey <jackey1510@gmail.com>
2017-04-19 15:18:18 +0100

    start scene

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
+
+    ScreenFader sf;
+    GameObject title;
+
+    // Use this for initialization
+    void Start()
+    {
+        sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
+        title = GameObject.FindGameObjectWithTag("Title");
+    }
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            SceneManager.LoadScene("intro", LoadSceneMode.Single);
+            StartCoroutine(fadeOut());
         }
 
+    }
+
+    private IEnumerator fadeOut()
+    {
+        title.SetActive(false);
+        yield return StartCoroutine(sf.FadeToBlack());
+        SceneManager.LoadScene("intro", LoadSceneMode.Single);
+        //yield return StartCoroutine(sf.FadeToClear());
     }
 }
